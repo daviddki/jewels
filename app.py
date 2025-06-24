2be53e926d305b942336a320e251824300dc990a
David Kim <davidk18@uci.edu>
2025-06-23 21:41:35 -0700

    update urls

--- app.py
+++ app.py
@@ -4,19 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('project1.html')
-
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
+    return render_template('jewels.html')
 
 @app.route('/project1')
 def project1():
-    return render_template('project1.html')
+    return render_template('jewels.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
